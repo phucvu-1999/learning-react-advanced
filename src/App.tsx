df9a6bf74d122740d160e24a9b78274b5fb77b95
Tien Phuc <leesinvn123@gmail.com>
2022-05-12 22:58:06 +0700

    Learning useMemo react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
-import React, { useState, useCallback } from "react";
-import Content from "./Components/Content";
+import React, { useState, useMemo } from "react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState<
+    { product: string; price: number }[]
+  >([]);
+  const [product, setProduct] = useState("");
+  const [price, setPrice] = useState("");
 
-  const increaseCount = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
+  const total = useMemo(() => {
+    const caluculateTotal = products.reduce(
+      (result: number, prod: { product: string; price: number }) => {
+        console.log("Re-calculate ...");
+
+        const finalResult = result + prod.price;
+        return finalResult;
+      },
+      0
+    );
+
+    return caluculateTotal;
+  }, [products]);
+
+  const handleAddItem = () => {
+    setProducts([...products, { product, price: +price }]);
+  };
 
   return (
     <div className="App">
-      <Content increase={increaseCount} />
-      <p>{count}</p>
+      <div>
+        Product: <input onChange={(e) => setProduct(e.target.value)} />
+      </div>
+      <div>
+        Price: <input onChange={(e) => setPrice(e.target.value)} />
+      </div>
+      Total: {total}
+      <br />
+      <button onClick={handleAddItem}>Add</button>
+      <ul>
+        {products.map((prod) => (
+          <li key={Math.random().toString()}>
+            {prod.product} - {prod.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
